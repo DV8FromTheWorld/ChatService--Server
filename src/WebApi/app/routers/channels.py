88229eae92d0cbd004716e45476c0a35acf8c383
Elijah Hursey <elijahnhursey@gmail.com>
2021-09-11 14:38:16 -0400

    basic implementation of some initial endpoints

--- src/WebApi/app/routers/channels.py
+++ src/WebApi/app/routers/channels.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, status
+from fastapi.responses import JSONResponse
 
 router = APIRouter(
     prefix='/api/v1/channels',
@@ -9,3 +10,18 @@ router = APIRouter(
 @router.get("/")
 async def get_channels():
     return {"msg": "Hi from channels controller"}
+
+
+@router.get("/{channelId}")
+async def get_channel(channelId):
+    return {"channelId": channelId}
+
+
+@router.get("/{channelId}/messages")
+async def get_channel_messages(channelId, limit: int = 100, offset: int = 0):
+    return {"channelId": channelId, "messages": ["msg1", "msg2"], "limit": limit, "offset": offset}
+
+
+@router.post("/{channelId}/messages")
+async def post_message(channelId):
+    return JSONResponse(status_code=status.HTTP_200_OK)
--- src/WebApi/app/routers/guilds.py
+++ src/WebApi/app/routers/guilds.py
@@ -9,3 +9,8 @@ router = APIRouter(
 @router.get("/")
 async def get_guilds():
     return {"msg": "Hi from guilds controller"}
+
+
+@router.get("/{guildId}")
+async def get_guilds(guildId):
+    return {"guildId": guildId}
